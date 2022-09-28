8071d1e0eb8f1ddb96aabe2d44b1c88fad2daffa
anfernee84 <eduard.rogulin@outlook.com>
2022-09-28 19:28:33 +0300

    add_invisibility_of_button

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { renderGallery } from './renderFunction';
 
-let inputForm = document.querySelector('#search-form');
-let loadMoreButton = document.querySelector('#load-more-button');
-let gallery = document.querySelector('.gallery');
+const inputForm = document.querySelector('#search-form');
+const loadMoreButton = document.querySelector('#load-more-button');
+const gallery = document.querySelector('.gallery');
+let sumHits;
 let page = 1;
-let sumHits = 0;
 let currentQuery = '';
 let lightbox = new SimpleLightbox('.photo-card a', {
   captions: true,
@@ -20,11 +20,12 @@ inputForm.addEventListener('submit', submitHandler);
 
 async function submitHandler(e) {
   e.preventDefault();
+  page = 1;
   currentQuery = e.currentTarget.searchQuery.value;
   if (!currentQuery) return;
   const response = await getPictures(currentQuery, 1);
   if (response.total > 0) {
-    sumHits += response.hits.length;
+    sumHits = response.hits.length;
     console.log(response);
     loadMoreButton.hidden = false;
     Notiflix.Notify.success(`Hooray! We found ${response.total} images.`);
@@ -48,7 +49,7 @@ async function moreButtonHandler() {
   const response = await getPictures(currentQuery, page);
   console.log(response);
   sumHits += response.hits.length;
-  if (stopFlag == 400 || sumHits >= response.totalHits) {
+  if (stopFlag == 400 || sumHits > response.totalHits) {
     loadMoreButton.hidden = true;
     Notiflix.Notify.info('This is the end...');
     return;
